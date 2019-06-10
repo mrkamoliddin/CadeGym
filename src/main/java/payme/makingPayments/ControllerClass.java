c348eb201d2925a446b0963babac893bd251b018
Uktam Nimatov <uktam.nimatov@gmail.com>
2019-06-10 16:51:23 +0500

    edit controllerClass for testing purpose

--- src/main/java/payme/makingPayments/ControllerClass.java
+++ src/main/java/payme/makingPayments/ControllerClass.java
@@ -1,13 +1,10 @@
 package payme.makingPayments;
 
-import jdk.nashorn.internal.objects.annotations.Constructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.jws.WebParam;
 import java.util.ArrayList;
 import java.util.List;
 
